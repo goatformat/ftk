d2d60f6bafc5cb26b9a4cbf333a9229ad989a54f
Kirk Scheibelhut <kjs@scheibo.com>
2021-09-30 23:19:29 -0700

    Actually fix reload off by one

--- index.ts
+++ index.ts
@@ -323,7 +323,7 @@ export const CARDS: { [name: string]: Data } = {
 
       // We can only set at most max cards before discarding, dependent on open zones and hand size
       const hand = d.hand.filter(id => ID.decode(id).type === 'Spell');
-      const max = Math.min(5 - state.spells.length - h, hand.length - 1);
+      const max = Math.min(5 - state.spells.length - h, hand.length, d.hand.length - 1);
       for (let n = 1; n < max; n++) {
         for (const set of isubsets(d.hand, n)) {
           if (set.some(j => ID.decode(d.hand[j]).type === 'Monster')) continue;
@@ -547,7 +547,7 @@ export const CARDS: { [name: string]: Data } = {
 
       // We can only set at most max cards before reloading, dependent on open zones and hand size
       const hand = d.hand.filter(id => ID.decode(id).type === 'Spell');
-      const max = Math.min(5 - state.spells.length - h, hand.length - 1);
+      const max = Math.min(5 - state.spells.length - h, hand.length, d.hand.length - 1);
       for (let n = 1; n <= max; n++) {
         for (const set of isubsets(d.hand, n)) {
           if (set.some(j => ID.decode(d.hand[j]).type === 'Monster')) continue;
