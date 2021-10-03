05ffbedb27634744b76e4c1161fa252d79250389
Kirk Scheibelhut <kjs@scheibo.com>
2021-10-02 21:46:58 -0700

    fix counter labels css

--- html/ui.ts
+++ html/ui.ts
@@ -38,9 +38,12 @@ export const HANDLERS: { [name: string]: {} } = {
   'Sangan': {
   },
   'Spell Reproduction': {
+    // does nothing unless condition is passed (2+ in hand, 1+ in grave)
+    // if only one option does that one option, otherwise fades out invalid options and lets choose
+    // progress to state with choice = want to restruction index.ts to pass in choices instead!
   },
   'Thunder Dragon': {
-
+    // if choice, pass in instead of generate
   },
   'Toon Table of Contents': {
   },
@@ -53,6 +56,7 @@ export const HANDLERS: { [name: string]: {} } = {
 const num = (window.location.hash && +window.location.hash.slice(1)) ||
   (window.location.search && +window.location.search.slice(1)) || 1;
 const state = State.create(new Random(Random.seed(num)));
+state.summon('L3' as any);
 
 const content = document.getElementById('content')!;
 const div = createElement('div');
